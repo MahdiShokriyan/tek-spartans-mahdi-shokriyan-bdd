c3f46e796e3812898d1751d3d82672a9652e7512
mahdishokriyan <mahdi.shokriyan@gmail.com>
2024-08-08 18:53:05 -0400

    added logger

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -2,6 +2,12 @@ package tek.bdd.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.apache.logging.log4j.LogManager;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,7 +15,14 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.apache.logging.log4j.Logger;
+
+
+import static java.util.logging.LogManager.getLogManager;
+
+
 public class BaseSetup {
+    private static final Logger LOGGER = LogManager.getLogger(BaseSetup.class);
     //WebDriver to how access in every method
     private static WebDriver driver;
     private Properties properties;
@@ -17,18 +30,20 @@ public class BaseSetup {
     //Find the full path to file
     //FileInputStream to read the file
     //Properties and load the FileInputStream to the properties
-    public BaseSetup(){
+    public BaseSetup() {
 
-       try {
-           String configFilePath = System.getProperty("user.dir") +
-                   "/src/test/resources/configs/dev-config.properties";
-           File file = new File(configFilePath);
-           FileInputStream fileInputStream = new FileInputStream(file);
-           properties = new Properties();
-           properties.load(fileInputStream);
-       }catch (IOException e){
-           throw new RuntimeException("Something wrong with config file ", e);
-       }
+        try {
+            String configFilePath = System.getProperty("user.dir") +
+                    "/src/test/resources/configs/qa-config.properties";
+            LOGGER.info("Reading Config file " + configFilePath);
+            File file = new File(configFilePath);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            LOGGER.error("Error reading config file", e);
+            throw new RuntimeException("Something wrong with config file ", e);
+        }
     }
 
     //Getter for WebDriver
@@ -37,13 +52,26 @@ public class BaseSetup {
     }
 
     public void setupBrowser() {
-     /*   WebDriver instance
-          Navigation to retail App
-          maximize
-          and implicitly wait
-      */
-
-        driver = new ChromeDriver();
+        String browserType = properties.getProperty("ui.browser");
+        boolean isHeadless = Boolean.parseBoolean(properties.getProperty("ui.browser.headless"));
+        if (browserType.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            if (isHeadless)
+                options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        } else if (browserType.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            if (isHeadless)
+                options.addArguments("--headless");
+            driver = new EdgeDriver(options);
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            if (isHeadless)
+                options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
+        } else {
+            throw new RuntimeException("Choose correct browser between Chrome, edge and firefox");
+        }
         String url = properties.getProperty("ui.url");
         driver.get(url);
         driver.manage().window().maximize();
